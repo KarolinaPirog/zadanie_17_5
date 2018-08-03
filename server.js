685c8deca23c488819b64aef14cea6418f359dc5
Karolina Piróg <karolina.swieca@wp.pl>
2018-08-03 11:46:54 +0200

    add pug in package.json, add new file and add app.set in server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views','./views');
+
 app.use('/store', function(req, res, next){
     console.log('Jestem pośrednikiem przy żądaniu do /store');
     next();
@@ -13,6 +16,11 @@ app.get('/', function (req, res) {
 app.get('/store', function (req, res) {
     res.send('To jest sklep');
 });
+
+app.get('/first-template', function(req, res){
+    res.render('first-template');
+});
+
 
 app.listen(3000);
 app.use(function (req, res, next) {
